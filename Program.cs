aa8a3f26e98d2338d902cd0d523deaba0c1075fb
oscaredu22 <oscaredu03@hotmail.com>
2018-06-05 00:23:51 -0500

    mi primer commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nuevaCalculadora
+{
+    class Program
+    {
+         private double memoria = 0;
+        public void sumar (double sumando1, double sumando2)
+        {
+            memoria = sumando1 + sumando2;
+            return memoria;
+        }
+        public void restar (double minuendo, double sutraendo)
+        {
+            memoria = minuendo - sutraendo;
+            return memoria;
+        }
+        public void multiplicar (double multiplicando, double multiplicador)
+        {
+            memoria = multiplicando * multiplicador;
+            return memoria;
+        }
+        public void dividir (double dividendo, double divisor)
+        {
+            memoria = dividendo + divisor;
+            return memoria;
+        }
+        public void double  Raiz(double radicando)
+    {  
+            memoria Math.Sqrt(radicando);
+     		return memoria;           
+    }
+        public void double  Raiz()
+    {    
+            memoria = Math.Sqrt(memoria);
+            return memoria;
+    }
+    }
+}
